8d48dccc22122c207df1b046dc0f129f5feaf3dd
Mathias Lund <mathias@graduateland.com>
2016-10-09 14:30:31 +0200

    added commands to package.json

--- server.js
+++ server.js
@@ -6,9 +6,5 @@ app.use(function(req, res, next) {
   require('./dist/server').default(req, res, next)
 })*/
 
-app.get('/', (req, res) => {
-    res.send("hey123");
-})
-
 const PORT = 9000
 app.listen(PORT, () => console.log('Listening on', PORT))
