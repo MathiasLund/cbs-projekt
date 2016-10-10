ec0c230d8ee127caff3f70d0740f02cbcd3100ad
Mathias Lund <mathias@graduateland.com>
2016-10-10 14:04:02 +0200

    updated package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.use(function(req, res, next) {
-  require('./dist/server').default(req, res, next)
+  require('./dist/server')(req, res, next)
 })
 
 const PORT = 9000
